3636260cfe04fd6273d61876326e408c56f5447a
ef3d0c3e <ef3d0c3e@pundalik.org>
2024-11-14 05:02:53 +0000

    Fix completion signatures

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -78,10 +78,11 @@ end
 
 function config.cmp.config()
 	local cmp = require("cmp")
+	local luasnip = require("luasnip")
 	cmp.setup({
 		snippet = {
 			expand = function(args)
-				require("luasnip").lsp_expand(args.body)
+				luasnip.lsp_expand(args.body)
 			end,
 		},
 		preselect = cmp.PreselectMode.None,
@@ -122,7 +123,37 @@ function config.cmp.config()
 			["<C-f>"] = cmp.mapping.scroll_docs(4),
 			["<C-Space>"] = cmp.mapping.complete(),
 			["<C-e>"] = cmp.mapping.abort(),
-			["<CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+			['<CR>'] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					if luasnip.expandable() then
+						luasnip.expand()
+					else
+						cmp.confirm({
+							select = true,
+						})
+					end
+				else
+					fallback()
+				end
+			end),
+			["<Tab>"] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_next_item()
+				elseif luasnip.locally_jumpable(1) then
+					luasnip.jump(1)
+				else
+					fallback()
+				end
+			end, { "i", "s" }),
+			["<S-Tab>"] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_prev_item()
+				elseif luasnip.locally_jumpable(-1) then
+					luasnip.jump(-1)
+				else
+					fallback()
+				end
+			end, { "i", "s" }),
 		}),
 		sources = cmp.config.sources({
 			{ name = "nvim_lsp" },
