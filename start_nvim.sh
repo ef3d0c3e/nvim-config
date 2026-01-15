42c0ed48325cdeb7e0a1f8610b6ff6584d8d4308
ef3d0c3e <ef3d0c3e@pundalik.org>
2026-01-15 09:25:45 +0100

    Startup script

--- start_nvim.sh
+++ start_nvim.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
-#[ -z $NVIM ] && nvim --listen ~/.cache/nvim/server${RANDOM}.pipe $* || nvim --server $NVIM --remote $*
-nvim $*
+
+if [ -z "${NVIM}" ]; then
+	nvim --listen ~/.cache/nvim/server${RANDOM}.pipe "$@"
+else
+	nvim --server "${NVIM}" --remote "$@"
+fi
